39432f273d0200b0cca2796cd97610d346ea95f1
ebell495 <ericbell495@live.com>
2023-05-27 16:13:36 -0700

    feat: integrate mayhem

---
+++ fuzz/fuzz_targets/html2text-fuzz.rs
@@ -0,0 +1,18 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use html2text::*;
+
+fuzz_target!(|data: &[u8]| {
+    // fuzzed code goes here
+    if data.len() > 2 {
+        let width: usize = usize::from(data[0]);
+        if width >= data.len() || width <= 0{
+            from_read(&(data[1..]), data.len());
+        } else {
+            from_read(&(data[1..]), width);
+        }
+
+    }
+
+});
